8e76ffc0c630801e71f07c640801af352bba1efc
SimonFinnigan <sifinnigan1@gmail.com>
2022-05-06 19:54:01 +0100

    test suite connected to data base and am now speaking with insomnia

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,7 +1,21 @@
+const app = require('../app')
+const request = require('supertest')
+const connection = require('../db/connection')
+const seed = require('../db/seeds/seed')
+const testData = require('../db/data/test-data')
+
+beforeEach(() => seed(testData))
+afterAll(() => connection.end())
+
 describe('TOPICS', () => {
   describe('GET /topics', () => {
     test('status: 200 - should respond with an array of objects, each of which should have the properties "slug" and "description".', () => {
-      return app.get('/topics').status(200)
+      return request(app)
+        .get('/topics')
+        .expect(200)
+        .then(({ body }) => {
+          console.log(body)
+        })
     })
   })
 })
--- app.js
+++ app.js
@@ -1,4 +1,8 @@
 const express = require('express')
 const app = express()
 
+app.get('/topics', (req, res) => {
+  res.status(200).send({ msg: 'Hello World!' })
+})
+
 module.exports = app
