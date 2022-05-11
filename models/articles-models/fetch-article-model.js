5c8ec8d50c4816be005520893b772cf8d1fc6c9c
SimonFinnigan <sifinnigan1@gmail.com>
2022-05-11 11:31:08 +0100

    commen_count feature request functioning and tested

--- models/articles-models/fetch-article-model.js
+++ models/articles-models/fetch-article-model.js
@@ -1,8 +1,16 @@
 const db = require('../../db/connection')
 
-const fetchArticle = (articleId) => {
+const fetchArticle = (article_id) => {
   return db
-    .query(`SELECT * FROM articles WHERE article_id = $1`, [articleId])
+    .query(
+      `SELECT a.*, COUNT(c.comment_id)::INT AS comment_count 
+      FROM articles AS a 
+      LEFT JOIN comments AS c 
+      ON a.article_id = c.article_id 
+      WHERE a.article_id = $1 
+      GROUP BY a.article_id`,
+      [article_id]
+    )
     .then(({ rows }) => {
       if (!rows[0]) {
         return Promise.reject({
