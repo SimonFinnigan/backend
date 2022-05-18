53b133ee9a5fbfa975e125ecd1d0b63757821b94
SimonFinnigan <sifinnigan1@gmail.com>
2022-05-18 16:40:00 +0100

    articles queries functioning and tested

--- models/articles-models/fetch-articles-model.js
+++ models/articles-models/fetch-articles-model.js
@@ -1,9 +1,47 @@
 const db = require('../../db/connection')
 
-const fetchArticles = () => {
-  return db.query(`SELECT * FROM articles`).then(({ rows }) => {
-    return rows
-  })
+const fetchArticles = (sort_by = 'created_at', order = 'desc', topic) => {
+  if (
+    ![
+      'article_id',
+      'title',
+      'topic',
+      'author',
+      'created_at',
+      'votes',
+      'comment_count',
+    ].includes(sort_by)
+  ) {
+    return Promise.reject({ status: 400, msg: 'Invalid sort query' })
+  }
+
+  if (!['asc', 'desc'].includes(order)) {
+    return Promise.reject({ status: 400, msg: 'Invalid order query' })
+  }
+
+  if (
+    !['cats', 'mitch', 'coding', 'football', 'cooking', undefined].includes(
+      topic
+    )
+  ) {
+    return Promise.reject({ status: 400, msg: 'Invalid topic query' })
+  }
+ 
+  const topicSort = topic === undefined ? '' : `WHERE topic = '${topic}'`
+
+  return db
+    .query(
+      `SELECT articles.*, 
+      COUNT(comments.article_id)::INT AS comment_count 
+      FROM articles
+      LEFT JOIN comments 
+      ON articles.article_id = comments.article_id   
+      ${topicSort} GROUP BY articles.article_id 
+      ORDER BY ${sort_by} ${order};`
+    )
+    .then(({ rows: articles }) => {
+      return articles
+    })
 }
 
 module.exports = fetchArticles
