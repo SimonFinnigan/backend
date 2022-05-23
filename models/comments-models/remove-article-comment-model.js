5058636a889b9724cc8a84fbdc38383add71c6e3
SimonFinnigan <sifinnigan1@gmail.com>
2022-05-23 15:06:11 +0100

    issues with testing now fixed and comment can now be deleted. tested.

--- models/comments-models/remove-article-comment-model.js
+++ models/comments-models/remove-article-comment-model.js
@@ -1,29 +1,22 @@
 const db = require('../../db/connection')
 
-const removeArticleComment = (comment_id) => {
+const removeArticleComment = (commentId) => {
   return db
     .query(
       `
-    DELETE FROM comments
-    WHERE comment_id = $1;`,
-      [comment_id]
+			DELETE FROM comments
+			WHERE comment_id = $1;`,
+      [commentId]
     )
-    .then(({ rows }) => {
-      return rows[0]
-    })
-}
-
-const checkCommentExists = (comment_id) => {
-  return db
-    .query('SELECT * FROM comments WHERE comment_id = $1;', [comment_id])
-    .then(({ rows }) => {
-      if (rows.length === 0)
+    .then((deleteConf) => {
+      const { rowCount } = deleteConf
+      if (rowCount === 0) {
         return Promise.reject({
           status: 404,
           msg: 'Comment not found',
         })
-      else return rows
+      } else return
     })
 }
 
-module.exports = { removeArticleComment, checkCommentExists }
+module.exports = { removeArticleComment }
